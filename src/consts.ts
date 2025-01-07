aac15529e7d743312e772b353817c92d4c8db0a4
Alessandro Lisi <alessandro.lisi@tmceurope.com>
2025-01-07 14:28:35 +0100

    fix: site image

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'Level11';
 export const SITE_DESCRIPTION = 'Unlock AI’s Potential in Your Applications';
 export const SITE_URL = 'https://level11.software';
-// export const SITE_IMAGE = '/og.png';
+export const SITE_IMAGE = '/site_image.png';
