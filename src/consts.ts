8ce483c921bfdd8fb00d13f48fb51798d68504d2
Alessandro Lisi <alessandro.lisi@tmceurope.com>
2025-01-07 14:26:44 +0100

    feat: favicon, removed social preview

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = 'Atom - A Tailus template';
-export const SITE_DESCRIPTION = 'Modern free Tailwind CSS, Astro landing page template for startups | built with Tailus Themer';
-export const SITE_URL = 'https://atom.tailus.io';
-export const SITE_IMAGE = '/og.png';
+export const SITE_TITLE = 'Level11';
+export const SITE_DESCRIPTION = 'Unlock AI’s Potential in Your Applications';
+export const SITE_URL = 'https://level11.software';
+// export const SITE_IMAGE = '/og.png';
